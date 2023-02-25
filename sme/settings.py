0b1efb8205489201b9665f6bdf7e29f3e36d1f93
Lucky Okirie <97410457+emmanuel1304@users.noreply.github.com>
2023-02-25 11:17:52 -0600

    Update settings.py

--- sme/settings.py
+++ sme/settings.py
@@ -137,9 +137,6 @@ cloudinary.config(
 )
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static/"),
-)
-
+STATIC_ROOT = 'static'
 LOGIN_REDIRECT_URL = '/home/'
 LOGIN_URL = '/login/'
